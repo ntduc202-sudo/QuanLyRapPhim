3049c6d7b4dcf8dbf3c8ff0882a7d2467f517cab
ntduc202 <ntduc.202@gmail.com>
2026-06-03 22:18:00 +0700

    GUI admin/user

--- src/main/java/com/nhom6/user/UserApp.java
+++ src/main/java/com/nhom6/user/UserApp.java
@@ -1,4 +1,36 @@
 package com.nhom6.user;
 
-public class UserApp {
+import javax.swing.*;
+import java.awt.*;
+
+public class UserApp extends JFrame {
+    public UserApp() {
+        setTitle("User - Dat Ve Xem Phim");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(5, 1, 10, 10));
+
+        JButton viewMovieButton = new JButton("Xem phim dang chieu");
+        JButton searchMovieButton = new JButton("Tim phim");
+        JButton bookingButton = new JButton("Dat ve");
+        JButton paymentButton = new JButton("Thanh toan");
+        JButton exitButton = new JButton("Thoat");
+
+        add(viewMovieButton);
+        add(searchMovieButton);
+        add(bookingButton);
+        add(paymentButton);
+        add(exitButton);
+
+        viewMovieButton.addActionListener(e -> JOptionPane.showMessageDialog(this, "Goi MovieService"));
+        searchMovieButton.addActionListener(e -> JOptionPane.showMessageDialog(this, "Goi MovieService tim phim"));
+        bookingButton.addActionListener(e -> JOptionPane.showMessageDialog(this, "Goi BookingService"));
+        paymentButton.addActionListener(e -> JOptionPane.showMessageDialog(this, "Goi PaymentService"));
+        exitButton.addActionListener(e -> System.exit(0));
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new UserApp().setVisible(true));
+    }
 }
