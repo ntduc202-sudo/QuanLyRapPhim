0eb381d0a498f9d1ce100881fb40a346315d313c
ntduc202 <ntduc.202@gmail.com>
2026-06-03 22:18:00 +0700

    GUI admin/user

--- src/main/java/com/nhom6/service/BookingService.java
+++ src/main/java/com/nhom6/service/BookingService.java
@@ -1,4 +1,15 @@
 package com.nhom6.service;
 
 public class BookingService {
+    public void bookTicket() {
+        System.out.println("Xu ly dat ve");
+    }
+
+    public void displayBookings() {
+        System.out.println("Hien thi danh sach ve da dat");
+    }
+
+    public void cancelBooking() {
+        System.out.println("Huy dat ve");
+    }
 }
