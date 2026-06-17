75d4da00be78971e8f1ae9f140aafb6884adb08d
hangthanh611 <thithuhangluc@gmail.com>
2026-06-17 12:45:57 +0700

    add BookingService

--- src/main/java/com/nhom6/service/BookingService.java
+++ src/main/java/com/nhom6/service/BookingService.java
@@ -1,15 +1,50 @@
 package com.nhom6.service;
 
+import com.nhom6.model.*;
+import com.nhom6.utils.IdGenerator;
+
 public class BookingService {
-    public void bookTicket() {
-        System.out.println("Xu ly dat ve");
+    public Ticket bookTicket(Customer customer, Movie movie, ShowTime showTime, Seat seat) {
+        if (customer == null || movie == null || showTime == null || seat == null) {
+            return null;
+        }
+
+        if (movie.getStatus() != MovieStatus.DANG_CHIEU) {
+            return null;
+        }
+
+        double finalPrice = calculateFinalPrice(showTime.getBasePrice(), seat.getSeatType(), customer.getCustomerType());
+
+        return new Ticket(
+                IdGenerator.generateTicketId(),
+                customer,
+                movie,
+                showTime,
+                seat,
+                finalPrice,
+                PaymentStatus.UNPAID
+        );
     }
 
-    public void displayBookings() {
-        System.out.println("Hien thi danh sach ve da dat");
-    }
+    private double calculateFinalPrice(double basePrice, SeatType seatType, CustomerType customerType) {
+        double price = basePrice;
 
-    public void cancelBooking() {
-        System.out.println("Huy dat ve");
+        if (seatType == SeatType.VIP) {
+            price += 30000;
+        }
+
+        if (seatType == SeatType.COUPLE) {
+            price += 50000;
+        }
+
+        if (customerType == CustomerType.STUDENT) {
+            price *= 0.9;
+        }
+
+        if (customerType == CustomerType.VIP) {
+            price *= 0.8;
+        }
+
+        return price;
     }
 }
