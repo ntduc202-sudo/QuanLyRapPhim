233114d7ff13fc294ae40e176361b1e9d51ab5e2
huyenn2905 <168351568+huyenn2905@users.noreply.github.com>
2026-06-17 14:38:58 +0700

    fix movieservice

--- src/main/java/com/nhom6/service/MovieService.java
+++ src/main/java/com/nhom6/service/MovieService.java
@@ -18,9 +18,38 @@ public class MovieService {
         fileRepository.saveAllMovies(movies);
     }
 
+    public void updateMovie(Movie oldMovie, Movie newMovie) {
+        List<Movie> movies = fileRepository.loadMovies();
+
+        for (int i = 0; i < movies.size(); i++) {
+            if (movies.get(i).getMovieId().equalsIgnoreCase(oldMovie.getMovieId())) {
+                movies.set(i, newMovie);
+                break;
+            }
+        }
+
+        fileRepository.saveAllMovies(movies);
+    }
+
+    public void deleteMovie(Movie movie) {
+        List<Movie> movies = fileRepository.loadMovies();
+        movies.removeIf(m -> m.getMovieId().equalsIgnoreCase(movie.getMovieId()));
+        fileRepository.saveAllMovies(movies);
+    }
+
     public boolean isDuplicateMovieId(String movieId) {
         for (Movie movie : fileRepository.loadMovies()) {
             if (movie.getMovieId().equalsIgnoreCase(movieId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isDuplicateMovieIdForUpdate(String movieId, Movie selectedMovie) {
+        for (Movie movie : fileRepository.loadMovies()) {
+            if (!movie.getMovieId().equalsIgnoreCase(selectedMovie.getMovieId())
+                    && movie.getMovieId().equalsIgnoreCase(movieId)) {
                 return true;
             }
         }
